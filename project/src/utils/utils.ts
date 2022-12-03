c73a82094468e75bb636cd8165ac5ccf11eedb6b
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-12-03 17:21:15 +0300

    Реализовал функциональность из технического задания

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -1,10 +1,17 @@
+import { cities } from '../const';
 import { City } from '../types/city';
 import {RoomOffer} from './../types/offer';
-
 
 export const calculateStarRating = (rating: number): number => Math.round(rating) * 20;
 
 export const getOffersByCity = (offers: RoomOffer[], city: City): RoomOffer[] => offers.filter((offer) => city.name === offer.city.name);
+
+export const getRandomCity = () => {
+  const min = 0;
+  const max = cities.length;
+  const randomIndex = Math.floor(Math.random() * (max - min)) + min;
+  return cities[randomIndex];
+};
 
 export const formatDate = (date: string): string => {
   const d = new Date(date);
